2dee8f274b21d209d47b5a20d80e2880b36e53d2
Vivian Chang <cyintzu@gmail.comgit config --global user.name Vivian>
2023-09-12 11:23:19 -0700

    add sign in function with firebase

--- src/App.js
+++ src/App.js
@@ -1,34 +1,55 @@
-import Directory from "./components/directory/directory.component.js";
+import Home from "./routes/home/home.component.js";
+import Navigation from "./routes/navigation/navigation.component.js";
+import SignIn from "./routes/signin/signin.component.js";
+import { Routes, Route } from "react-router-dom";
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>This is the shop page!</h1>
+    </div>
+  );
+};
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-  return <Directory categories={categories} />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="signin" element={<SignIn />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
+
+// const categories = [
+//   {
+//     id: 1,
+//     title: "hats",
+//     imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+//   },
+//   {
+//     id: 2,
+//     title: "jackets",
+//     imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+//   },
+//   {
+//     id: 3,
+//     title: "sneakers",
+//     imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+//   },
+//   {
+//     id: 4,
+//     title: "womens",
+//     imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+//   },
+//   {
+//     id: 5,
+//     title: "mens",
+//     imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+//   },
+// ];
+// return <Directory categories={categories} />;
